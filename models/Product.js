425946a2e6759ab3816af7d67eb8ae7dc35a7ceb
Kayla <sullenspartyof3@gmail.com>
2023-09-28 10:50:46 -0700

    Worked on product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoincrement: true,
+    },
+    product_name: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataaType.DECIMAL,
+      allowNull: false,
+      //validate: decimal
+    },
+    stock: {
+      type: DataType.INTEGER,
+      allownull: false,
+      defaultValue: 10,
+      //validate: numeric,
+    },
+    category_id: {
+      type: DataType.INTEGER,
+      //references: category_id,
+    }
   },
   {
     sequelize,
